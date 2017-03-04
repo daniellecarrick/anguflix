4ec3b3a7c35a5870a927936a85dd3aa2c9c5552d
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-03-04 20:34:08 +0200

    Can add and remove movies from collection and search

--- public/movieController.js
+++ public/movieController.js
@@ -1,3 +1,17 @@
 app.controller('movieController', function($scope, movieFactory) {
   $scope.movies = movieFactory.movies;
+  $scope.collection = movieFactory.collection;
+  $scope.addToCollection = function(movie) {
+    //console.log($scope.movies[index]);
+    $scope.collection.push(movie);                //Adds the selected movie to the collections array
+    var index = $scope.movies.indexOf(movie);
+    $scope.movies.splice(index,1);                //Removes selected movie from movies array
+   // console.log($scope.collection);
+  }
+
+  $scope.removeFromCollection = function(movie) { //Adds the selected movie to the movies array
+    $scope.movies.push(movie); //Removes selected movie from collections array
+    var index = $scope.collection.indexOf(movie);
+    $scope.collection.splice(index,1);
+  }
 });
